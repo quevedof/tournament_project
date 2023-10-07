09b6d847a8c21a25393749e5ae80c195b749ec34
Zheng Hung Chuah <zheng_hung@hotmail.com>
2023-10-07 17:35:43 +0100

    Create tournament models

--- tournament/admin.py
+++ tournament/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
-from .models import Feature, Tournament
+from .models import Participant, Tournament, TournamentParticipant, Match, MatchParticipant
+
 # Register your models here.
 
-admin.site.register(Feature)
-admin.site.register(Tournament)
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'team_name')
+    search_fields = ('email', 'team_name')
 
+@admin.register(Tournament)
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'generated_key', 'number_of_teams', 'date')
+    search_fields = ('name', 'generated_key')
 
+@admin.register(TournamentParticipant)
+class TournamentParticipantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tournament', 'participant')
+    list_filter = ('tournament',)
+    search_fields = ('participant__email', 'participant__team_name')
+
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tournament', 'date', 'winning_participant', 'next_match')
+    list_filter = ('tournament',)
+    search_fields = ('tournament__name', 'date')
+
+@admin.register(MatchParticipant)
+class MatchParticipantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'match', 'participant', 'score')
+    list_filter = ('match',)
+    search_fields = ('participant__email', 'participant__team_name')
--- tournament/models.py
+++ tournament/models.py
@@ -2,11 +2,38 @@ from django.db import models
 
 # Create your models here.
 
-class Feature(models.Model):
-    name = models.CharField(max_length=100)
-    details = models.CharField(max_length=500)
-    
-    
+class Participant(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    team_name = models.CharField(max_length=255, unique=True)
+
 class Tournament(models.Model):
-    name = models.CharField(max_length=100)
-    num_of_teams = models.IntegerField()
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    generated_key = models.CharField(max_length=255)
+    number_of_teams = models.IntegerField()
+    date = models.DateTimeField()
+
+class TournamentParticipant(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    
+    class Meta:
+        unique_together = ('tournament', 'participant')
+
+class Match(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    winning_participant = models.ForeignKey(Participant, on_delete=models.SET_NULL, null=True)
+    next_match = models.ForeignKey('self', on_delete=models.SET_NULL, null=True)
+
+class MatchParticipant(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    score = models.IntegerField(null=True)
+    
+    class Meta:
+        unique_together = ('match', 'participant')
